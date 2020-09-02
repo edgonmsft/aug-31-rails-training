c34ac93c1965e0e22d9d221b721f114b8576c4f3
Ilana Corson <ilana.corson@gmail.com>
2020-09-02 16:00:18 -0700

    add new movie form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,28 @@
 class MoviesController < ApplicationController
   def index
     movies = Movie.all
-    render locals: { movies: movies } 
+    render locals: { movies: movies }
   end
 
   def show
     movie = Movie.find(params[:id])
     render locals: { movie: movie }
+  end
+
+  def new
+    movie = Movie.new
+    render locals: { movie: movie }
+  end
+
+  def create
+    movie = Movie.create(movie_params)
+
+    redirect_to "/movies/#{movie.id}", locals: { movie: movie }
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :director, :year)
   end
 end
--- test/system/movies_system_test.rb
+++ test/system/movies_system_test.rb
@@ -25,4 +25,56 @@ class MoviesSystemTest < ApplicationSystemTestCase
     assert_text movie_2.title
     assert_text movie_2.director
   end
+
+  test "creating a new movie" do
+    # As a user,
+    #   When I visit "/movies",
+    #   I see a link/button to add a new movie.
+    visit "/movies"
+
+    assert_button "Add New Movie"
+
+    click_button "Add New Movie"
+    #   When I click on "Add New Movie",
+
+    assert_current_path "/movies/new"
+    #   I am on a new page,
+
+    assert_selector ".new_movie"
+    #   and I see a form to add a new movie.
+    #
+    fill_in :movie_title, with: "Drop Dead Fred"
+    fill_in :movie_director, with: "Ilana Corson"
+    fill_in :movie_year, with: "1992"
+
+    click_on "Add Movie"
+    #   I fill in the title, director name and year
+    #   I click "Add Movie",
+    assert_text "Drop Dead Fred"
+    assert_text "Ilana Corson"
+    assert_text "1992"
+    #   and I am redirected to the page for that movie,
+    #   and I see the title, director name and year.
+  end
+
+  test "edit a movie" do
+    movie = Movie.create(title: "Jumanji", director: "Ilana Corson", year: "2017")
+
+    visit "/movies/#{movie.id}"
+
+    assert_button "Edit #{movie.title}"
+
+    click_button "Edit #{movie.title}"
+
+    assert_current_path "/movies/#{movie.id}/edit"
+
+    assert_selector ".edit_movie"
+    
+    fill_in :movie_year, with: "2019"
+
+    click_on "Update Movie"
+
+    assert_text "2019"
+    assert_no_text "2017"
+  end
 end
